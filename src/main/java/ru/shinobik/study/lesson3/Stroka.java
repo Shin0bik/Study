c119444a32ed6807eed65e676f9a2e9d7cbc31e3
Shin0bik <shinobik2677@mail.ru>
2022-12-07 01:08:06 +0300

    calculateProduct

--- src/main/java/ru/shinobik/study/lesson3/Stroka.java
+++ src/main/java/ru/shinobik/study/lesson3/Stroka.java
@@ -1,5 +1,7 @@
 package ru.shinobik.study.lesson3;
 
+import javafx.scene.control.IndexRange;
+
 public class Stroka {
     public static void main(String[] args) {
         String stroka = new String("S1");
@@ -14,6 +16,34 @@ public class Stroka {
         System.out.println(stroka4 == null);
         String substring = stroka2.substring(2, 5);
         System.out.println(substring);
+        stringOut();
+        moreOrLess();
     }
-    public static void lesson3Stroka(String s, int n)
+
+
+    //написать новый метод psv : На вход даётся строка, и мой метод должен распечатать эту строку n раз.
+    public static void stringOut(String s, int n) {
+        while (n > 0) {
+            System.out.println(s);
+            n--;
+        }
+    }
+
+    //на вход даётся строка, на выходе на экран печатается, если строка больше 10 символов то печатается всё, что начинается с 5 символа этой строки,
+    public static void moreOrLess(String s) {
+        s.length();
+        if (s.length() < 10) {
+            System.out.println(s);
+        } else {
+            System.out.println(s.substring(5));
+
+        }
+    }
+
+    //На вход даётся строка и распечатать количество слов в этой строке.
+    public static void wordCounter(String str) {
+        String[] str;
+        str.split(" ", 0);
+        System.out.println(str.length);
+    }
 }
